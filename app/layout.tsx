49918222f4c4e39a3c6ed602b8d16cb7a1f976d2
Mirabelle <25381101+mirabelle514@users.noreply.github.com>
2025-05-26 07:45:57 -0500

    Update File Structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout() {
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="not-found" />
+        <Stack.Screen name="tabs" options={{ headerShown: false }} />
+        <Stack.Screen name="notfound" />
       </Stack>
       <StatusBar style="auto" />
     </>
